03ed7578fb5028f0bde8f0f20eb381f976d75bb2
usorfaitheloho <usorfaitheloho@gmail.com>
2024-09-28 21:44:37 +0300

    Add openAPI Setup for controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { AppService } from './app.service';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 
-@ApiTags('Data Operations')
+@ApiTags('App')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -15,25 +15,4 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get('fetch-profiles')
-  @ApiOperation({ summary: 'Fetch profiles from the database' })
-  @ApiResponse({ status: 200, description: 'Successfully fetched profiles' })
-  @ApiResponse({ status: 500, description: 'Internal server error' })
-  async fetchProfiles() {
-    const data = await this.appService.fetchProfiles();
-    console.log(data);
-    return 'test data from table profiles: ' + JSON.stringify(data, null, 2);
-  }
-
-  @Get('fetch-table/:table_name')
-  @UsePipes(new ZodValidationPipe(tableNameSchema))
-  @ApiOperation({ summary: 'Fetch data from a specific table' })
-  @ApiResponse({ status: 200, description: 'Successfully fetched data' })
-  @ApiResponse({ status: 500, description: 'Internal server error' })
-  @ApiResponse({ status: 404, description: 'Invalid table name' })
-  async fetchTable(@Param('table_name') table_name: ValidTableName) {
-    const data = await this.appService.fetchTable(table_name);
-    console.log(data);
-    return 'test data from table ' + table_name + ': '+ JSON.stringify(data, null, 2);
-  }
 }
