eff0ab1cdce5be0a04f0cdac295ceb4303b3ddc8
Rasmus <ahtavarasmus@gmail.com>
2024-10-05 15:45:39 +0300

    made oauth module, modified ngrok and docker proxy and got gpt to work one time:D

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SupabaseModule } from './supabase/supabase.module';
+import { AuthModule } from './oauth2/oauth2.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), SupabaseModule],
+  imports: [ConfigModule.forRoot(), SupabaseModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
