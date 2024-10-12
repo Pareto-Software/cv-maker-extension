cfa69ede8edc47d3916e1933fdb2c913a013915a
Samu Toljamo <samu.toljamo@paretosoftware.fi>
2024-10-12 19:56:46 +0300

    Add mock data

--- src/sheet/dtos.ts
+++ src/sheet/dtos.ts
@@ -4,9 +4,23 @@ export type StatusValue =
   | 'flexible_start'
   | 'unavailable';
 
+export type Month =
+  | 'Jan'
+  | 'Feb'
+  | 'Mar'
+  | 'Apr'
+  | 'May'
+  | 'Jun'
+  | 'Jul'
+  | 'Aug'
+  | 'Sep'
+  | 'Oct'
+  | 'Nov'
+  | 'Dec';
+
 export interface CellValueDTO {
   year: number;
-  month: number;
+  month: Month;
   reservationPercentage: number;
   status: StatusValue;
 }
