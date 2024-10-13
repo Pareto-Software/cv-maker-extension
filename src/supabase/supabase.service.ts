9ce16b1decea0f3530fd67b0b155976e5388b561
usorfaitheloho <usorfaitheloho@gmail.com>
2024-10-13 08:30:41 +0300

    Add supabaseclientprovider for exports

--- src/supabase/supabase.service.ts
+++ src/supabase/supabase.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { SupabaseClient } from '@supabase/supabase-js';
-import { SupabaseClientProvider } from './supabase-client.provider';
+import { SupabaseClientProvider } from './supabase-client.provider'; 
 import { Database } from './database.types';
 import { ValidTableName } from './table-name.schema';
 
@@ -28,6 +28,47 @@ export class SupabaseService {
     return data;
   }
 
+  async getEmployeesSkillsAndProject(){
+    const { data: profiles, error: profileError } = await this.supabase
+      .from('profiles')
+      .select('user_id, first_name')
+      .throwOnError();
+
+    if (profileError) {
+      throw new Error(`Error fetching profiles : ${profileError.message}`);
+      }
+
+    const { data: skills, error: skillsError } = await this.supabase
+      .from('skills')
+      .select('user_id, skill')
+      .throwOnError();
+     
+
+    if (skillsError) {
+      throw new Error(`Error fetching skills : ${skillsError.message}`);
+    }
+
+    const { data: projects, error: projectsError } = await this.supabase
+      .from('projects')
+      .select('user_id, name');
+      
+    if (projectsError) {
+      throw new Error(`Error fetching projects : ${projectsError.message}`);
+    }
+
+    const employees = profiles.map((profile) => {
+      return {
+        name: profile.first_name,
+        skills: skills
+          .filter((skill) => skill.user_id === profile.user_id)
+          .map((skill) => skill.skill),
+        projects: projects
+          .filter((project) => project.user_id === profile.user_id)
+          .map((project) => project.name),
+      };
+    });
+    return employees; 
+  }
   async insertData(table: ValidTableName, values: any) {
     const { data, error } = await this.supabase.from(table).insert(values);
     if (error) {
