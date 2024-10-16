328b6e02c3a27076a13ea45b913923fed829e438
unknown <kush17.aswani@gmail.com>
2024-10-16 11:52:16 +0300

    ZZ fixing rebase merge conflict
         ck c412764 added test for allocation.service
    
    fixing merge conflict from rebase

--- src/allocation/allocation.controller.ts
+++ src/allocation/allocation.controller.ts
@@ -1,9 +1,29 @@
-import { Controller, Get, Headers, HttpCode} from '@nestjs/common';
-import {AllocationService} from './allocation.service';
+import { Controller, Get, Headers, HttpCode, Param, NotFoundException,UnauthorizedException} from '@nestjs/common'
+import {AllocationService,AllocationResponseDTO} from './allocation.service';
 
 @Controller('allocation')
 export class AllocationController {
   constructor(private readonly allocationService: AllocationService) {}
+
+  @Get(':name')
+  async getAllocationByName(
+    @Param('name') name: string,
+    @Headers() headers: Record<string, string>,
+  ): Promise<AllocationResponseDTO> {
+    const access_token = headers.authorization?.replace('Bearer ', '').trim();
+    if (!access_token) {
+      throw new UnauthorizedException('Access token is missing or invalid');
+    }
+    try {
+      const data = await this.allocationService.getAllocationByName(name,access_token);
+      return data;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new Error('An unexpected error occurred.');
+    }
+  }
 
   @Get('sheetdata')
   @HttpCode(200)
