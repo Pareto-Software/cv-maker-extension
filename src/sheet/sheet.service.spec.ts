f5758b7dcde0051249c0e8d2b0ce0122c6efbd3e
Rasmus <ahtavarasmus@gmail.com>
2024-10-22 15:06:47 +0300

    commiting some craph

--- src/sheet/sheet.service.spec.ts
+++ src/sheet/sheet.service.spec.ts
@@ -1,26 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SheetService } from './sheet.service';
-import { ConfigService } from '@nestjs/config';
 
 describe('SheetService', () => {
   let service: SheetService;
 
-    beforeEach(async () => {
+  beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-          providers: [
-            SheetService,
-            {
-              provide: ConfigService,
-              useValue: {
-                // Mock methods if necessary
-              },
-            },
-          ],
-        }).compile();
+      providers: [SheetService],
+    }).compile();
 
-        service = module.get<SheetService>(SheetService);
-      });
-    it('should be defined', () => {
+    service = module.get<SheetService>(SheetService);
+  });
+
+  it('should be defined', () => {
     expect(service).toBeDefined();
   });
 });
