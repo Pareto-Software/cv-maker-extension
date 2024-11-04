40500279b557a15a4b1915906bef422374c45c3e
unknown <kush17.aswani@gmail.com>
2024-11-04 20:55:09 +0200

    remove requested line

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,6 @@ async function bootstrap() {
     .setTitle('CV maker extension')
     .setDescription('cv maker extension for pareto software')
     .setVersion('1.0')
-    .addServer('https://localhost:3000', 'Local development server')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, { ...document, openapi: '3.1.0' });
