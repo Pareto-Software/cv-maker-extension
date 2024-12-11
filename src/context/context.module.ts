60cfa99c54e22dcfdc8c53d24acfd0fa4cd76729
Samu Toljamo <samu.toljamo@paretosoftware.fi>
2024-12-11 10:11:11 +0200

    Remove import

--- src/context/context.module.ts
+++ src/context/context.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { ContextController } from './context.controller.js';
 import { ContextService } from './context.service.js';
 import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from '../oauth2/oauth2.module.js';
 
 @Module({
-  imports: [ConfigModule, AuthModule],
+  imports: [ConfigModule],
   controllers: [ContextController],
   providers: [ContextService],
 })
